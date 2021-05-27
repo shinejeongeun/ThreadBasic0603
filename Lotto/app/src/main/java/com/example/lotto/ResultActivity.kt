97fc9b34fe8df968c73fd7caf4e727b7395084db
J-E shine <amy5549@naver.com>
2021-05-27 15:16:11 +0900

    Updated lotto ball images

--- Lotto/app/src/main/java/com/example/lotto/ResultActivity.kt
+++ Lotto/app/src/main/java/com/example/lotto/ResultActivity.kt
@@ -2,6 +2,7 @@ package com.example.lotto
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.Toast
 
 class ResultActivity : AppCompatActivity() {
@@ -9,6 +10,24 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        Toast.makeText(this, "Result Activity", Toast.LENGTH_LONG).show()
+        val result = intent.getIntegerArrayListExtra("result") ?: return
+
+        val result_sorted = result?.sorted()
+
+        val lottoBallImageStartId = R.drawable.ball_01
+
+        val imageView1 = findViewById<ImageView>(R.id.imageView1)
+        val imageView2 = findViewById<ImageView>(R.id.imageView2)
+        val imageView3 = findViewById<ImageView>(R.id.imageView3)
+        val imageView4 = findViewById<ImageView>(R.id.imageView4)
+        val imageView5 = findViewById<ImageView>(R.id.imageView5)
+        val imageView6 = findViewById<ImageView>(R.id.imageView6)
+
+        imageView1.setImageResource(lottoBallImageStartId + (result_sorted[0] - 1))
+        imageView2.setImageResource(lottoBallImageStartId + (result_sorted[1] - 1))
+        imageView3.setImageResource(lottoBallImageStartId + (result_sorted[2] - 1))
+        imageView4.setImageResource(lottoBallImageStartId + (result_sorted[3] - 1))
+        imageView5.setImageResource(lottoBallImageStartId + (result_sorted[4] - 1))
+        imageView6.setImageResource(lottoBallImageStartId + (result_sorted[5] - 1))
     }
 }
