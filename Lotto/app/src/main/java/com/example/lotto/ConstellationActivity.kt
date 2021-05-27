0d76067b9095c9e63277e13d47a762115b8e11d1
J-E shine <amy5549@naver.com>
2021-05-27 13:25:43 +0900

    Random number generation

--- Lotto/app/src/main/java/com/example/lotto/ConstellationActivity.kt
+++ Lotto/app/src/main/java/com/example/lotto/ConstellationActivity.kt
@@ -1,7 +1,9 @@
 package com.example.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 
 class ConstellationActivity : AppCompatActivity() {
@@ -9,6 +11,9 @@ class ConstellationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
 
-        Toast.makeText(this, "Constellation Activity", Toast.LENGTH_LONG).show()
+        val btnConstellationResult = findViewById<Button>(R.id.btnGoResult)
+        btnConstellationResult.setOnClickListener {
+            startActivity(Intent(this@ConstellationActivity, ResultActivity::class.java))
+        }
     }
 }
--- Lotto/app/src/main/java/com/example/lotto/NameActivity.kt
+++ Lotto/app/src/main/java/com/example/lotto/NameActivity.kt
@@ -1,7 +1,9 @@
 package com.example.lotto
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.Toast
 
 class NameActivity : AppCompatActivity() {
@@ -9,6 +11,14 @@ class NameActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
 
-        Toast.makeText(this, "Name Activity", Toast.LENGTH_LONG).show()
+        val btnNameResult = findViewById<Button>(R.id.btnGoResultName)
+        btnNameResult.setOnClickListener {
+            startActivity(Intent(this@NameActivity, ResultActivity::class.java))
+        }
+
+        val btnback = findViewById<Button>(R.id.btnBack)
+        btnback.setOnClickListener {
+            finish()
+        }
     }
 }
