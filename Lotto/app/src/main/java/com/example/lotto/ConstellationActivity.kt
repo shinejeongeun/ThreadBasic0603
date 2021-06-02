0ffbee36aa01ea24081364f28dce59ba1230b51c
J-E shine <amy5549@naver.com>
2021-06-02 12:55:06 +0900

    HashCodeBasedOnDatePicker

--- Lotto/app/src/main/java/com/example/lotto/ConstellationActivity.kt
+++ Lotto/app/src/main/java/com/example/lotto/ConstellationActivity.kt
@@ -4,56 +4,63 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CalendarView
 import android.widget.DatePicker
 import android.widget.TextView
 import java.text.SimpleDateFormat
 import java.util.*
 import kotlin.collections.ArrayList
 
-fun getShuffledLottoNumbersFromHash(str: String): MutableList<Int> {
-    val list = mutableListOf<Int>()
-
-    for (number in 1..45) {
-        list.add(number)
-    }
-
-    val targetString = SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-SS", Locale.KOREA).format(Date())+str
-    list.shuffle(Random(targetString.hashCode().toLong()))
-
-    return list.subList(0,6)
-}
-
 class ConstellationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_constellation)
 
-        val result = intent.getIntegerArrayListExtra("result") ?: return
-        val strConstellation = intent.getStringExtra("constellation")
-
-        val result_sorted = result?.sorted()
-
-
         val datePicker = findViewById<DatePicker>(R.id.datePicker)
-        val btnGoResultConstell = findViewById<Button>(R.id.btnGoResult)
         val txtConstell = findViewById<TextView>(R.id.txtCostell)
-        txtConstell.text = makeConstellationString(datePicker.month,datePicker.dayOfMonth)
+        txtConstell.text = makeConstellationString(datePicker.month, datePicker.dayOfMonth)
+        val btnGoResult = findViewById<Button>(R.id.btnGoResult)
 
-        btnGoResultConstell.setOnClickListener {
+        btnGoResult.setOnClickListener {
             val intent = Intent(this,ResultActivity::class.java)
-            intent.putIntegerArrayListExtra("result", ArrayList(getShuffledLottoNumbers(txtConstell.text.toString())))
-            intent.putExtra("constellation", makeConstellationString(datePicker.month + 1, datePicker.dayOfMonth))
+            intent.putIntegerArrayListExtra("result",
+                    ArrayList(getShuffledLottoNumbersFromHash(txtConstell.text.toString(),datePicker.month,datePicker.dayOfMonth)))
+            intent.putExtra("constellation", makeConstellationString(datePicker.month,datePicker.dayOfMonth))
+            val date = ""+datePicker.year + "년" + datePicker.month +"월" + datePicker.dayOfMonth+"일"
+            intent.putExtra("date",date)
             startActivity(intent)
         }
+        val calendar = Calendar.getInstance()
+
+        datePicker.init(calendar.get(Calendar.YEAR),calendar.get(Calendar.MONTH),calendar.get(Calendar.DAY_OF_MONTH),
+                object: CalendarView.OnDateChangeListener,DatePicker.OnDateChangedListener{
+                    override fun onSelectedDayChange(view: CalendarView, year: Int, month: Int, dayOfMonth: Int)
+                    {}
+                    override fun onDateChanged(view: DatePicker?, year: Int, monthOfYear: Int, dayOfMonth: Int) {
+                        txtConstell.text = makeConstellationString(datePicker.month,datePicker.dayOfMonth)
+                    }
+
+                }
+
+
+        )
+
     }
+    fun getShuffledLottoNumbersFromHash(str: String, month: Int, dayOfMonth: Int): MutableList<Int> {
+        val list = mutableListOf<Int>()
 
+        for (number in 1..45) {
+            list.add(number)
+        }
 
+        val target = "${month + 1}${String.format("%02d",dayOfMonth)}"+ str
+        list.shuffle(Random(target.hashCode().toLong()))
+
+        return list.subList(0,6)
     }
-
-    private fun makeConstellationString(month: Int, dayOfMonth: Int): String {
-        val target = "${month}${String.format("%02d",dayOfMonth)}".toInt()
-
-        when (target) {
+    private fun makeConstellationString(month: Int, dayOfMonth: Int): CharSequence?  {
+        val target = "${month + 1}${String.format("%02d",dayOfMonth)}".toInt()
+        when(target){
             in 101..119 -> return "염소자리"
             in 120..218 -> return "물병자리"
             in 219..320 -> return "물고기자리"
@@ -64,11 +71,12 @@ class ConstellationActivity : AppCompatActivity() {
             in 723..822 -> return "사자자리"
             in 823..923 -> return "처녀자리"
             in 924..1022 -> return "천칭자리"
-            in 1023..1122-> return "전갈자리"
-            in 1123..1224-> return "사수자리"
+            in 1023..1122 -> return "전갈자리"
+            in 1123..1224 -> return "사수자리"
             in 1225..1231 -> return "염소자리"
             else -> return "기타별자리"
+
         }
 
-
     }
+}
--- Lotto/app/src/main/java/com/example/lotto/ResultActivity.kt
+++ Lotto/app/src/main/java/com/example/lotto/ResultActivity.kt
@@ -12,31 +12,35 @@ class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
-
-        val result = intent.getIntegerArrayListExtra("result") ?: return
-        val sConstellation = intent.getStringExtra("constellation")
-        val result_sorted = result?.sorted()
-
-        sConstellation?.let {
-            val resultLabel = findViewById<TextView>(R.id.resultLabel)
-            resultLabel.text = "${sConstellation}dml ${SimpleDateFormat("yyyy년 MM월 dd일").format(Date())}"
+        val result = intent.getIntegerArrayListExtra("result") //?: return
+        val constellation = intent.getStringExtra("constellation")
+        val date = intent.getStringExtra("date")
+        result?.let {
+            updateLottoBallImages(result.sortedBy { it })
         }
+        constellation?.let {
+            val resultLabel = findViewById<TextView>(R.id.resultLabel)
+            resultLabel.text = "${constellation}의 ${date} 로또 번호입니다"
+        }
+    }
+    private fun updateLottoBallImages(result_sorted:List<Int>) {
+        val lottoBallImageStartId = R.drawable.ball_01
         val imageView1 = findViewById<ImageView>(R.id.imageView1)
         val imageView2 = findViewById<ImageView>(R.id.imageView2)
         val imageView3 = findViewById<ImageView>(R.id.imageView3)
         val imageView4 = findViewById<ImageView>(R.id.imageView4)
         val imageView5 = findViewById<ImageView>(R.id.imageView5)
         val imageView6 = findViewById<ImageView>(R.id.imageView6)
+        imageView1.setImageResource(lottoBallImageStartId + result_sorted!![0] - 1)
+        imageView2.setImageResource(lottoBallImageStartId + result_sorted[1] - 1)
+        imageView3.setImageResource(lottoBallImageStartId + result_sorted[2] - 1)
+        imageView4.setImageResource(lottoBallImageStartId + result_sorted[3] - 1)
+        imageView5.setImageResource(lottoBallImageStartId + result_sorted[4] - 1)
+        imageView6.setImageResource(lottoBallImageStartId + result_sorted[5] - 1)
 
-        val lottoBallImageStartId = R.drawable.ball_01
-        val lottoBallImageId2 = R.drawable.ball_02
-        val lottoBallImageId3 = R.drawable.ball_03
-
-        imageView1.setImageResource(lottoBallImageStartId + (result_sorted[0] - 1))
-        imageView2.setImageResource(lottoBallImageStartId + (result_sorted[1] - 1))
-        imageView3.setImageResource(lottoBallImageStartId + (result_sorted[2] - 1))
-        imageView4.setImageResource(lottoBallImageStartId + (result_sorted[3] - 1))
-        imageView5.setImageResource(lottoBallImageStartId + (result_sorted[4] - 1))
-        imageView6.setImageResource(lottoBallImageStartId + (result_sorted[5] - 1))
+        // 146
+/*       val lottoBallImageId2 = R.drawable.ball_02 // 150
+        val lottoBallImageId3 = R.drawable.ball_03 // 148*/
     }
+
 }
